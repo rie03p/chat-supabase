99558b3042f92b7de1e80cfbf96718aa095389c3
rie03p <hgrt.0822@gmail.com>
2025-12-10 12:09:32 +0000

    feat: devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+set -exo pipefail
+
+if ! grep -q mise ~/.zshrc; then
+    # shellcheck disable=SC2016
+    echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
+fi
+
+mise trust
+mise install -y
+eval "$(mise activate zsh)"
